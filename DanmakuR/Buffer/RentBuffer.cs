950a1545cf4dd9b2e1b1e74855a34ebb29572b1f
Fa鸽 <1207247008@qq.com>
2022-02-01 04:10:01 +0800

    SimpleSegment

---
+++ DanmakuR/Buffer/RentBuffer.cs
@@ -0,0 +1,31 @@
+﻿using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace DanmakuR.Buffer
+{
+	public struct RentBuffer : IDisposable
+	{
+		private byte[]? buff = null;
+		public byte[] Buff
+		{
+			[MethodImpl(MethodImplOptions.AggressiveOptimization)]
+			get => buff ?? throw new ObjectDisposedException(nameof(RentBuffer));
+		}
+
+		public void Reset(int size)
+		{
+			if (size != 0)
+				buff = ArrayPool<byte>.Shared.Rent(size);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveOptimization)]
+		public void Dispose()
+		{
+			if (buff != null)
+			{
+				ArrayPool<byte>.Shared.Return(buff);
+				buff = Array.Empty<byte>();
+			}
+		}
+	}
+}
