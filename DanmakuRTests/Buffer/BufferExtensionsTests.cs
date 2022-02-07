82740feb447b696b697ce3e58ff657c0f83254e6
Fa鸽 <1207247008@qq.com>
2022-02-07 08:03:24 +0800

    创建测试

---
+++ DanmakuRTests/Buffer/BufferExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DanmakuR.Buffer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanmakuR.Buffer.Tests
+{
+	[TestClass()]
+	public class BrotliTests
+	{
+		[TestMethod()]
+		public void DecompressTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		public void BrokenTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		public void MultipleSegmentTest()
+		{
+
+		}
+	}
+}
