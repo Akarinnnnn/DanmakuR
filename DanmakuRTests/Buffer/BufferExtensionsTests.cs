a11174691086f1f3e301464e64c1d7d5216935a7
Fa鸽 <1207247008@qq.com>
2022-02-09 23:43:06 +0800

    deflate测试

--- DanmakuRTests/Buffer/BufferExtensionsTests.cs
+++ DanmakuRTests/Buffer/BufferExtensionsTests.cs
@@ -93,4 +93,67 @@ namespace DanmakuR.Buffer.Tests
 			Assert.IsTrue(StreamHelpers.AreEqual(src, new ReadOnlySequenceStream(ref seq)), "内容");
 		}
 	}
+
+	[TestClass()]
+	public class DeflateTests
+	{
+		[TestInitialize]
+		public void PrepareEnvironment()
+		{
+			Environment.CurrentDirectory = Path.GetDirectoryName(typeof(BrotliTests).Assembly.Location)!;
+		}
+
+		[DataRow("data/BDanmakuProtocol.cs")]
+		[DataRow("data/BufferExtensions.cs")]
+		[DataRow("data/Handshake2.cs")]
+		[DataRow("data/SimpleSegment.cs")]
+		[TestMethod()]
+		public void DecompressTest(string srcName)
+		{
+			using FileStream src = File.OpenRead(srcName);
+			using FileStream br = File.OpenRead(srcName + ".deflate");
+
+			using RentBuffer data = new();
+			data.Reset(8192);
+			Memory<byte> compressed = data.Memory[..br.Read(data.Span)];
+			ReadOnlySequence<byte> seq = new(compressed);
+			seq.DecompressDeflate();
+			Assert.IsTrue(StreamHelpers.AreEqual(src, new ReadOnlySequenceStream(ref seq)), "内容");
+		}
+
+		[DataRow("data/BDanmakuProtocol.cs")]
+		[TestMethod]
+		public void BrokenTest(string srcName)
+		{
+			using FileStream br = File.OpenRead(srcName + ".broken.deflate");
+			using RentBuffer data = new();
+			data.Reset(8192);
+			Memory<byte> compressed = data.Memory[..br.Read(data.Span)];
+			ReadOnlySequence<byte> seq = new(compressed);
+			Assert.ThrowsException<InvalidDataException>(() => seq.DecompressDeflate());
+		}
+
+		[DataRow("data/BDanmakuProtocol.cs")]
+		[DataRow("data/BufferExtensions.cs")]
+		[DataRow("data/Handshake2.cs")]
+		[DataRow("data/SimpleSegment.cs")]
+		[TestMethod]
+		public void MultipleSegmentTest(string srcName)
+		{
+			using FileStream src = File.OpenRead(srcName);
+			using FileStream br = File.OpenRead(srcName + ".deflate");
+
+			using RentBuffer data = new();
+			data.Reset(8192);
+			Memory<byte> compressed = data.Memory[..br.Read(data.Span)];
+			int midpoint = compressed.Length / 2;
+			SimpleSegment first = new(compressed[..midpoint], 0);
+			SimpleSegment last = first.SetNext(compressed[midpoint..], midpoint);
+			ReadOnlySequence<byte> seq = new(first, 0, last, last.Memory.Length);
+			seq.DecompressDeflate();
+
+			Assert.IsTrue(StreamHelpers.AreEqual(src, new ReadOnlySequenceStream(ref seq)), "内容");
+		}
+
+	}
 }
