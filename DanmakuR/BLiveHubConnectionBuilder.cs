42fa8ca1398725ffbdf8ad49fc043bf5c347683d
Fa鸽 <1207247008@qq.com>
2022-07-13 23:29:43 +0800

    我再想想？

---
+++ DanmakuR/BLiveHubConnectionBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace DanmakuR
+{
+	public class BLiveHubConnectionBuilder : HubConnectionBuilder
+	{
+		public BLiveHubConnectionBuilder()
+		{
+			this.UseBLiveProtocol();
+			
+		}
+
+		public new HubConnection Build()
+		{
+			var connection = base.Build();
+
+			// Todo: 以后考虑注册一个函数，用来解决一个数据包含有多个信息的问题
+			// 现在的做法不符合HubProtocol不能有状态的准则
+			// 多线程也容易出现资源争用问题
+			return connection;
+		}
+	}
+}
